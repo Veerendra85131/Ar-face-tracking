67c43adf7f9a707bb66ff12c153c0f160aaa6027
Veerendra85131 <56646838+Veerendra85131@users.noreply.github.com>
2022-04-14 13:24:33 +0530

    Add files via upload

---
+++ Arcore face tracking/Assets/scripts/FaceController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARFaceManager))]
+public class FaceController : MonoBehaviour
+{
+  
+
+    private ARFaceManager arFaceManager;
+
+
+
+    private int swapCounter = 0;
+
+    [SerializeField]
+    public FaceMaterial[] materials;
+
+    void Awake()
+    {
+        arFaceManager = GetComponent<ARFaceManager>();
+
+      
+
+        arFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[0].Material;
+    }
+
+    public void Faceone()
+    {
+       
+
+        foreach (ARFace face in arFaceManager.trackables)
+        {
+            face.GetComponent<MeshRenderer>().material = materials[0].Material;
+        }
+
+     
+    }
+    public void Facetwo()
+    {
+       
+
+        foreach (ARFace face in arFaceManager.trackables)
+        {
+            face.GetComponent<MeshRenderer>().material = materials[1].Material;
+        }
+
+      
+    }
+    public void Facethree()
+    {
+        
+        foreach (ARFace face in arFaceManager.trackables)
+        {
+            face.GetComponent<MeshRenderer>().material = materials[2].Material;
+        }
+
+       
+    }
+  
+
+}
+
+[System.Serializable]
+public class FaceMaterial
+{
+    public Material Material;
+
+   
+}
---
+++ Arcore face tracking/Assets/scripts/Facemanger.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+
+namespace UnityEngine.XR.ARFoundation
+{
+    public class Facemanger : MonoBehaviour
+    {
+        [SerializeField]
+
+        public Material[] materials;
+
+       // public Facematerial[] materials;
+
+        private ARFaceManager aRFaceManager;
+        bool faceone, facetwo, facethree;
+        void Awake()
+        {
+            aRFaceManager = GetComponent<ARFaceManager>();
+            aRFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[1];
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if(faceone == true)
+            {
+                
+                aRFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[0];
+                faceone = false;
+            }
+
+            if (facetwo == true)
+            {
+                aRFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[1];
+                facetwo = false;
+            }
+            if (facethree == true)
+            {
+                aRFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[2];
+                facethree = false;
+            }
+        }
+
+
+
+        public void face1()
+        {
+            faceone = true;
+          //  aRFaceManager = GetComponent<ARFaceManager>();
+          //  aRFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[0];
+
+            //  Debug.Log("face1");
+        }
+        public void face2()
+        {
+            facetwo = true;
+            // aRFaceManager = GetComponent<ARFaceManager>();
+            //aRFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[1];
+            //   Debug.Log("face2");
+        }
+        public void face3()
+        {
+            facethree = true ;
+           // aRFaceManager = GetComponent<ARFaceManager>();
+           // aRFaceManager.facePrefab.GetComponent<MeshRenderer>().material = materials[2];
+          //  Debug.Log("face3");
+        }
+    }
+}
